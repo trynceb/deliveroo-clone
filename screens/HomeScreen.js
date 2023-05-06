d90caaa5c04bbce7e0df742da8ddc265365b0eee
Trynce Brandt <tjbrandt2013@gmail.com>
2023-05-05 20:51:19 -0400

    add icon image

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const HomeScreen = () => {
+  const navigation = useNavigation()
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false
+    })
+  }, [])
+
   return (
-    <View>
-      <Text className="text-red-500">HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text className="text-red-500">
+
+        {/* Header */}
+        <View>
+          <Image
+            source={{
+              uri: 'https://links.papareact.com/wru',
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+        </View>
+      </Text>
+    </SafeAreaView>
   )
 }
 export default HomeScreen
